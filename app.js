58faf41fdcb00c699cfebe08454aae997a0fb289
BitGourde Project <87956155+jimmy2167@users.noreply.github.com>
2023-04-13 10:14:46 +0000

    Added new feature

--- app.js
+++ app.js
@@ -1,70 +1,172 @@
-// Connect Wallet Button
-const connectButton = document.getElementById("connectButton");
-connectButton.addEventListener("click", async () => {
-	if (window.ethereum) {
-		try {
-			await window.ethereum.request({ method: "eth_requestAccounts" });
-			alert("Wallet connected!");
-		} catch (error) {
-			alert("Error connecting wallet.");
-			console.error(error);
-		}
-	} else {
-		alert("Please install MetaMask to use this interface.");
+const ipfs = new window.IpfsHttpClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
+const contractAddress = '0x123abc'; 0x8b9B5a9C58521ddE46dAa6b780c2b6aB607AC814
+const contractABI = [[
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "fileId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "bytes32",
+				"name": "fileHash",
+				"type": "bytes32"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "fileName",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "fileType",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "fileSize",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "fileDescription",
+				"type": "string"
+			}
+		],
+		"name": "FileAdded",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "bytes32",
+				"name": "_fileHash",
+				"type": "bytes32"
+			},
+			{
+				"internalType": "string",
+				"name": "_fileName",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_fileType",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_fileSize",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "_fileDescription",
+				"type": "string"
+			}
+		],
+		"name": "storeFile",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "fileCount",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "files",
+		"outputs": [
+			{
+				"internalType": "bytes32",
+				"name": "fileHash",
+				"type": "bytes32"
+			},
+			{
+				"internalType": "string",
+				"name": "fileName",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "fileType",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "fileSize",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "fileDescription",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
 	}
+]];
+
+window.addEventListener('load', async () => {
+  if (window.ethereum) {
+    window.web3 = new Web3(window.ethereum);
+    try {
+      await window.ethereum.enable();
+    } catch (error) {
+      console.error('User denied account access');
+    }
+  } else if (window.web3) {
+    window.web3 = new Web3(web3.currentProvider);
+  } else {
+    console.error('No Web3 provider detected');
+  }
+
+  const fileUploadForm = document.getElementById('file-upload-form');
+  const fileInput = document.getElementById('file-input');
+  fileUploadForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const file = fileInput.files[0];
+    const fileBuffer = await file.arrayBuffer();
+    const fileHash = await addFileToIPFS(fileBuffer);
+    const txHash = await addFileToBlockchain(fileHash);
+    const fileUrl = `https://ipfs.io/ipfs/${fileHash}`;
+    document.getElementById('file-hash').value = fileUrl;
+  });
 });
 
-// Upload File
-const uploadButton = document.getElementById("uploadButton");
-uploadButton.addEventListener("click", async (event) => {
-	event.preventDefault();
-	const file = document.getElementById("fileInput").files[0];
-	if (!file) {
-		alert("Please select a file to upload.");
-		return;
-	}
-	try {
-		const Web3 = require('web3');
-		const web3 = new Web3(window.ethereum);
-		const accounts = await web3.eth.getAccounts();
-		const ipfs = new IpfsHttpClient({ host: "ipfs.infura.io", port: 5001, protocol: "https" });
-		const ipfsFile = await ipfs.add(file);
-		alert(`File uploaded to IPFS with hash: ${ipfsFile.cid.toString()}`);
-	} catch (error) {
-		alert("Error uploading file.");
-		console.error(error);
-	}
-});
-
-// Download File
-const downloadButton = document.getElementById("downloadButton");
-downloadButton.addEventListener("click", async () => {
-	const ipfsHash = document.getElementById("ipfsHashInput").value;
-	if (!ipfsHash) {
-		alert("Please enter an IPFS hash.");
-		return;
-	}
-	try {
-		const ipfs = new IpfsHttpClient({ host: "ipfs.infura.io", port: 5001, protocol: "https" });
-		const stream = ipfs.cat(ipfsHash);
-		const blob = new Blob([await streamToArray(stream)]);
-		const url = URL.createObjectURL(blob);
-		const downloadLink = document.getElementById("downloadLink");
-		downloadLink.href = url;
-		downloadLink.download = ipfsHash;
-		downloadLink.click();
-	} catch (error) {
-		alert("Error downloading file.");
-		console.error(error);
-	}
-});
-
-// Helper function to convert a stream to an array
-function streamToArray(stream) {
-	return new Promise((resolve, reject) => {
-		const chunks = [];
-		stream.on("data", (chunk) => chunks.push(chunk));
-		stream.on("error", reject);
-		stream.on("end", () => resolve(Buffer.concat(chunks)));
-	});
+async function addFileToIPFS(fileBuffer) {
+  const filesAdded = await ipfs.add({ content: fileBuffer });
+  return filesAdded.cid.toString();
 }
+
+async function addFileToBlockchain(fileHash) {
+  const contract = new window.web3.eth.Contract(contractABI, contractAddress);
+  const accounts = await window.web3.eth.getAccounts();
+  const result = await contract.methods.storeFile(fileHash).send({ from: accounts[0] });
+  return result.transactionHash;
+}
+
